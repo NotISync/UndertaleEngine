da2aa1c2144294db626bb7ad5e5c1aa7bed2575c
Utsukuro <140878965+NotISync@users.noreply.github.com>
2026-07-14 07:58:56 +0800

    battle box changes

--- scripts/battle/battle_box.gd
+++ scripts/battle/battle_box.gd
@@ -3,27 +3,54 @@
 extends NinePatchRect
 class_name BattleBox
 
-@onready var target : Array = [offset_left, offset_top, offset_right, offset_bottom]
-@onready var outline : NinePatchRect = $outline
+@onready var target : Array = [offset_left + 5, offset_top + 5, offset_right - 5, offset_bottom - 5]
 var margin : Array 
 var resize_speed : float = 600
 var emit_resize = false
 var auto := true
+var resize_mode := 0
 signal resize_finished
 @onready var collisions : Array = [$collisions/left, $collisions/up, $collisions/right, $collisions/down]
 
+func insta_box_size(target : Array) -> void:
+	self.target = target
+	offset_left = target[0] - 5
+	offset_top = target[1] - 5
+	offset_right = target[2] + 5
+	offset_bottom = target[3] + 5
+	resize_finished.emit()
 
 func _process(delta : float) -> void:
-	outline.size = size + Vector2(6,6)
-	outline.self_modulate.a = Vector4(material.get_shader_parameter("bgcolor")).w
 	if(auto):
-		margin = [offset_left, offset_top, offset_right, offset_bottom]
+		margin = [offset_left + 5, offset_top + 5, offset_right - 5, offset_bottom - 5]
 		var spd = resize_speed * delta
 
-		for i in 4:
-			if abs(margin[i] - target[i]) <= spd: margin[i] = target[i]
-			elif margin[i] > target[i]: margin[i] -= spd
-			else: margin[i] += spd
+		match(resize_mode):
+			0:
+				for i in 4:
+					if abs(margin[i] - target[i]) <= spd: margin[i] = target[i]
+					elif margin[i] > target[i]: margin[i] -= spd
+					else: margin[i] += spd
+			1: 
+				for i in [0, 2]:
+					if abs(margin[i] - target[i]) <= spd: margin[i] = target[i]
+					elif margin[i] > target[i]: margin[i] -= spd
+					else: margin[i] += spd
+				if(margin[0] == target[0] && margin[2] == target[2]):
+					for j in [1, 3]:
+						if abs(margin[j] - target[j]) <= spd: margin[j] = target[j]
+						elif margin[j] > target[j]: margin[j] -= spd
+						else: margin[j] += spd
+			2:
+				for i in [1, 3]:
+					if abs(margin[i] - target[i]) <= spd: margin[i] = target[i]
+					elif margin[i] > target[i]: margin[i] -= spd
+					else: margin[i] += spd
+				if(margin[1] == target[1] && margin[3] == target[3]):
+					for j in [0, 2]:
+						if abs(margin[j] - target[j]) <= spd: margin[j] = target[j]
+						elif margin[j] > target[j]: margin[j] -= spd
+						else: margin[j] += spd
 
 		if margin == target && !emit_resize:
 			resize_finished.emit()
@@ -31,24 +58,23 @@ func _process(delta : float) -> void:
 		elif margin != target && emit_resize:
 			emit_resize = false
 
-		offset_left = margin[0]
-		offset_top = margin[1]
-		offset_right = margin[2]
-		offset_bottom = margin[3]
-
+		offset_left = margin[0] - 5
+		offset_top = margin[1] - 5
+		offset_right = margin[2] + 5
+		offset_bottom = margin[3] + 5
 
 	pivot_offset = size / 2.0
 	$corner_bottom.position = size - Vector2(5, 5)
-
-	collisions[0].shape.size = Vector2(5,size.y)
+	$corner_bottom_inner.position = size - Vector2(10, 10)
+	collisions[0].shape.size = Vector2(5,size.y-10)
 	collisions[0].position = collisions[0].shape.size / 2
-	collisions[1].shape.size = Vector2(size.x, 5)
+	collisions[1].shape.size = Vector2(size.x-10, 5)
 	collisions[1].position = collisions[1].shape.size / 2
-	collisions[2].shape.size = Vector2(5,size.y)
-	collisions[2].position = collisions[2].shape.size / 2 + Vector2(size.x - 5,0)
-	collisions[3].shape.size = Vector2(size.x, 5)
-	collisions[3].position = collisions[3].shape.size / 2 + Vector2(0,size.y - 5)
-
+	collisions[2].shape.size = Vector2(5,size.y-10)
+	collisions[2].position = collisions[2].shape.size / 2 + Vector2(size.x - 15,0)
+	collisions[3].shape.size = Vector2(size.x-10, 5)
+	collisions[3].position = collisions[3].shape.size / 2 + Vector2(0,size.y - 15)
+	
 #OLD WAY OF MANAGING COLLISIONS
 #	collisions[0].shape.extents = Vector2(480, 640)
 #	collisions[1].shape.extents = Vector2(640, 480)
@@ -69,14 +95,6 @@ func reset_box_size(resize_speed = 500) -> void:
 	self.resize_speed = resize_speed
 	target = [32, 250, 607, 390]
 	emit_resize = false
-
-func insta_box_size(target : Array) -> void:
-	self.target = target
-	offset_left = target[0]
-	offset_top = target[1]
-	offset_right = target[2]
-	offset_bottom = target[3]
-	resize_finished.emit()
 
 func get_top_left() -> Vector2:
 	return global_position
