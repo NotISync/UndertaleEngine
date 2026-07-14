82ec23c459c911c88577a1c365714b7f7f46e440
Utsukuro <140878965+NotISync@users.noreply.github.com>
2026-07-14 08:21:58 +0800

    debug wip

--- scripts/debug.gd
+++ scripts/debug.gd
@@ -4,6 +4,7 @@ class_name Debug
 @onready var text := $text
 var enabled := false
 var time_elasped := 0.01
+var t = 0
 
 func _ready():
 	vars.debug = self
@@ -14,6 +15,7 @@ func _process(delta):
 	debug()
 
 func debug():
+	#if(Input.is_action_just_pressed("change_soul_color")):
 	if(Input.is_action_just_pressed("debug") && settings.debug_enabled):
 		enabled = !enabled
 		visible = enabled
@@ -24,6 +26,8 @@ func debug():
 					(vars as vars).attack_manager.turn_num += 1
 			if(Input.is_action_just_pressed("left")):
 					(vars as vars).attack_manager.turn_num -= 1
+		if(Input.is_action_just_pressed("change_soul_color")):
+			vars.player_heart.heart_mode = 0 if(vars.player_heart.heart_mode == PlayerHeart.e_heart_mode.size()-1)else vars.player_heart.heart_mode+1
 		if(Input.is_action_just_pressed("reset_attack")):
 			if(is_instance_valid(vars.attack_manager)):
 				if(is_instance_valid((vars as vars).attack_manager.current_attack)):
@@ -33,8 +37,9 @@ func debug():
 				(vars as vars).attack_manager.current_attack.end_attack()
 				(vars as vars).attack_manager.delete_bullets.emit()
 		text.visible = true
-		var string = "Engine By Soldier\n"
-		string += "HP = Infinite\n"
+		#var string = "TP = " + str(vars.player_heart.tp) + "\n"
+		#string += "Invul Left = " + str(vars.player_heart.tp_timer)
+		var string = "HP = Infinite\n"
 		string += str("FPS = " + str(Engine.get_frames_per_second())) + "\n"
 		string += str("Timer = " + str(snapped(time_elasped, 0.01))) + "\n"
 		if(is_instance_valid(vars.attack_manager)):
@@ -45,6 +50,5 @@ func debug():
 				string += "Status : Attacking\n"
 				string += "Attack Counter : " + str(int((vars as vars).attack_manager.current_attack.current_frames)) + "\n"
 			string += "Current Turn : " + str((vars as vars).attack_manager.turn_num) + "\n"
-			string += "Bullet Instances: " + str((vars as vars).attack_manager.masks.get_child_count() - 2) + "\n"
+			string += "Bullet Instances: " + str((vars as vars).attack_manager.masks.get_child_count() - 3) + "\n"
 		text.text = string
-		
