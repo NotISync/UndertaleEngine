37137bca5d91e1aadbf077feec4b1900c4485d40
Utsukuro <140878965+NotISync@users.noreply.github.com>
2026-07-14 08:25:40 +0800

    base attack tweak

--- scripts/battle/attacks/attack_base.gd
+++ scripts/battle/attacks/attack_base.gd
@@ -3,21 +3,24 @@ class_name Attack
 
 var attack_started := false
 var current_frames := 0.0
-var frames := 120.0
+var frames := 0.0
 signal attack_finished
 
 func pre_attack():
-	vars.player_heart.heart_mode = PlayerHeart.e_heart_mode.red
-	vars.battle_box.set_box_size([244,250,399,390],500)
+	vars.battle_box.resize_mode = 1
+	vars.battle_box.set_box_size([250,250,390,390],500)
 	vars.player_heart.visible = true
-	vars.player_heart.global_position = Vector2(321, 324)
+	vars.player_heart.global_position = Vector2(320, 320)
 
 func start_attack():
 	vars.player_heart.input_enabled = true
 	attack_started = true
 
 func end_attack():
+	vars.battle_box.resize_mode = 2
 	vars.hud_manager.reset()
+	settings.player_save.get_weapon().turn_effect()
+	settings.player_save.get_armor().turn_effect()
 	attack_finished.emit()
 	queue_free()
 
