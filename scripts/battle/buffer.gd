0b83c59ef655031dab6fb9713e964760e7837bcd
Utsukuro <140878965+NotISync@users.noreply.github.com>
2026-07-14 08:25:01 +0800

    bullet mask tweak

--- scripts/battle/buffer.gd
+++ scripts/battle/buffer.gd
@@ -6,3 +6,13 @@ func _process(delta):
 		$masks/top_left.global_rotation = vars.battle_box.get_node("corner_top").global_rotation
 		$masks/bottom_right.global_position = vars.battle_box.get_node("corner_bottom").global_position
 		$masks/bottom_right.global_rotation = vars.battle_box.get_node("corner_bottom").global_rotation
+		$masks/box_mask/top_left.global_position = vars.battle_box.get_node("corner_top_inner").global_position
+		$masks/box_mask/top_left.global_rotation = vars.battle_box.get_node("corner_top_inner").global_rotation
+		$masks/box_mask/bottom_right.global_position = vars.battle_box.get_node("corner_bottom_inner").global_position
+		$masks/box_mask/bottom_right.global_rotation = vars.battle_box.get_node("corner_bottom_inner").global_rotation
+		$masks/box_mask/box.offset_left = vars.battle_box.offset_left
+		$masks/box_mask/box.offset_top = vars.battle_box.offset_top
+		$masks/box_mask/box.offset_right = vars.battle_box.offset_right
+		$masks/box_mask/box.offset_bottom = vars.battle_box.offset_bottom
+		$masks/box_mask/box.global_position = vars.battle_box.global_position
+		$masks/box_mask/box.rotation = vars.battle_box.rotation
